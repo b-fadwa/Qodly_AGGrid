b431ffe93d9fca8451afe91c98f1905b7a157eb7
Ayoub METWALLI <ayoub.metwalli@4d.com>
2025-10-11 09:39:38 +0100

    fix custom Style for cell

--- src/components/AgGrid/CustomCell.tsx
+++ src/components/AgGrid/CustomCell.tsx
@@ -1,6 +1,6 @@
 import { formatValue } from '@ws-ui/webform-editor';
 import { MdCheck, MdClose } from 'react-icons/md';
-//import { DataType, getStyle } from '@ws-ui/formatter';
+import { DataType, getStyle } from '@ws-ui/formatter';
 
 const CustomCell = ({ format, dataType, value }: { format: any; dataType: string; value: any }) => {
   switch (true) {
@@ -31,9 +31,16 @@ const CustomCell = ({ format, dataType, value }: { format: any; dataType: string
             ? formatValue(value, dataType, format)
             : value.toString()
           : value;
-      //const customStyle = format ? getStyle(dataType as DataType, format, value) : {};
+      const customStyle =
+        format && format !== 'icon' && format !== 'checkbox'
+          ? getStyle(dataType as DataType, format, value)
+          : {};
 
-      return <div /*style={customStyle}*/ className="cell whitespace-nowrap">{customValue}</div>;
+      return (
+        <div style={customStyle} className="cell whitespace-nowrap">
+          {customValue}
+        </div>
+      );
   }
 };
 
